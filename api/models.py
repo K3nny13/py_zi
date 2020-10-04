4779c3c71289cff842f45896fcea613195482090
Kenneth Cogzell <kenneth_cogzell@hotmail.com>
2020-10-04 19:04:17 +0200

    Added models in api/models.py

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Tutorial(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField(max_length=512)
+
+
+class Rating(models.Model):
+    tutorial = models.ForeignKey(Tutorial, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
+    value = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    class Meta:
+        unique_together = (('user', 'tutorial'),)
+        index_together = (('user', 'tutorial'),)
